6e4614e9b964fd9f01b8c664e72591e30191d94e
Андрей Кащей <66840541+Kashchei1406@users.noreply.github.com>
2022-07-31 21:14:38 +0200

    Create Header

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    printWidth: 120, // Длинна строки
+    singleQuote: true, // Одиночные ковычки
+    trailingComma: "es5", // Ставить ли запятую в конце тела объекта
+    bracketSpacing: true, // Пробел между телом объекта и фигурной скобкой
+    tabWidth: 2
+}
